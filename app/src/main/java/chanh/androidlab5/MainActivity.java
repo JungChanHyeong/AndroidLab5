ee50bc92cc3efedbd2d34dd498d84d2fd3ff70a7
JungChanHyeong <chanh327@gmail.com>
2015-12-07 01:00:16 +0900

    My Location

--- app/src/main/java/chanh/androidlab5/MainActivity.java
+++ app/src/main/java/chanh/androidlab5/MainActivity.java
@@ -1,31 +1,65 @@
 package chanh.androidlab5;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
-
-    static final LatLng SEOUL = new LatLng(37.56, 126.97);
     private GoogleMap map;
+    static final LatLng KOOKMIN = new LatLng(37.6121866, 126.9953410);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-        Marker seoul = map.addMarker(new MarkerOptions().position(SEOUL)
-                .title("Seoul"));
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 15));
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        map = mapFragment.getMap();
 
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(KOOKMIN, 15));
+
+        MyLocation.LocationResult locationResult = new MyLocation.LocationResult() {
+            @Override
+            public void gotLocation(Location location) {
+
+                String msg = "lon: " + location.getLongitude() + " -- lat: " + location.getLatitude();
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                drawMarker(location);
+
+            }
+        };
+
+        MyLocation myLocation = new MyLocation();
+        myLocation.getLocation(getApplicationContext(), locationResult);
+
     }
+
+
+    private void drawMarker(Location location) {
+
+        map.clear();
+        LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 17));
+        map.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
+
+        map.addMarker(new MarkerOptions()
+                .position(currentPosition)
+                .snippet("Lat:" + location.getLatitude() + "Lng:" + location.getLongitude())
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("현재위치"));
+    }
+
 }
